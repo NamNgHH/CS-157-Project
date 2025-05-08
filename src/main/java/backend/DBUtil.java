9d71fd088721b62393387a533db8a63697ae6913
Christian <christianher89@gmail.com>
2025-05-08 14:25:39 -0700

    Fixed authenticationfilter and registerservlet

--- src/main/java/backend/DBUtil.java
+++ src/main/java/backend/DBUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBUtil {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/fitness_db";
     private static final String DB_USER = "root";
-    private static final String DB_PASS = "Luckyher1$";
+    private static final String DB_PASS = "Your Password Here";
 
     static {
         try {
@@ -22,10 +22,10 @@ public class DBUtil {
     public static Connection getConnection() throws SQLException {
         try {
             Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
-            System.out.println("✅ DB connection established!");
+            System.out.println("DB connection established!");
             return conn;
         } catch (SQLException e) {
-            System.out.println("❌ DB connection failed: " + e.getMessage());
+            System.out.println("DB connection failed: " + e.getMessage());
             e.printStackTrace();
             throw e; // Rethrow the exception to be handled by the caller
         }
